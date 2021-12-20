7a0e5826ddc8f1095e86303d3adf73b50e551edf
zhibek <zhibek.ksmva@gmail.com>
2021-12-20 16:50:56 +0600

    changes to the app

--- python_group9_homework_44_Zhibek_Kasymova/urls.py
+++ python_group9_homework_44_Zhibek_Kasymova/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp import views as webapp_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', webapp_views.play)
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def play(request):
+    secret_numbers = [2, 3, 5, 7]
+    if request.method == 'GET':
+        return render(request, 'index.html')
+    elif request.method == 'POST':
+        bull_cow = []
+        count = ''
+        numbers = int(request.POST.get('numbers'))
+        numbers = list(map(int, ['numbers'][0].split()))
+        while True:
+            for i, j in zip(secret_numbers, numbers):
+                if j in secret_numbers:
+                    if j == i:
+                        bull_cow[0] += 1
+                        if bull_cow[0] == 4:
+                            count = "You got it right"
+                    else:
+                        bull_cow[1] += 1
+                        count = f"{bull_cow[0]} bulls, {bull_cow[1]} cows"
+        context = {
+            'numbers': request.POST.get('numbers'),
+            'count': count,
+            'bull_cow': bull_cow
+        }
+        return render(request, 'index.html', context)
